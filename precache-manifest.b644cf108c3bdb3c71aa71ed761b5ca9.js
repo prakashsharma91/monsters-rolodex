341d736e73848bc841f9c0fd605e991795e6d7b4
PrAkAsH <prakashsharmra91@gmail.com>
2020-04-16 03:27:01 +0530

    Updates

---
+++ precache-manifest.b644cf108c3bdb3c71aa71ed761b5ca9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68c638b5972d32f2e4115fb74cf8931e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "00d704e8d3d73f953a50",
+    "url": "/monsters-rolodex/static/css/main.57938892.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00d704e8d3d73f953a50",
+    "url": "/monsters-rolodex/static/js/main.0cd889bb.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
